7168cfc84f878a7b596798c0e1c346b824001b7f
Shai Grossman <72968565+TapChap@users.noreply.github.com>
2025-06-04 04:57:34 +0300

    REMOVE redundant .5 sec sleep statement

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import time, pyperclip, string_converter
-import keyboard as kw
+import keyboard as kb
 
 from pynput.keyboard import Key, Controller
 
@@ -10,8 +10,6 @@ from pynput.keyboard import Key, Controller
 # Special keys: enter, esc, space, tab, backspace, up, down, left, right, etc.
 
 def handle_hotkey(keyboard):
-    time.sleep(0.5)
-
     # using a different lib to handle shift & home keystrokes
     # since keyboard package couldn't do it
     keyboard.press(Key.shift)
@@ -20,29 +18,29 @@ def handle_hotkey(keyboard):
     keyboard.release(Key.home)
 
     # cut the bad text
-    kw.press_and_release('ctrl + x')
+    kb.press_and_release('ctrl + x')
     time.sleep(0.05)
 
-    # convert the bad to text to the correct language and paste it
+    # convert the bad text to the correct language and paste it
     text = pyperclip.paste()
-    kw.write(string_converter.convert(text))
+    kb.write(string_converter.convert(text))
 
     # change pc language
-    kw.press_and_release('alt + shift')
+    kb.press_and_release('alt + shift')
 
 def change_hotkey(new_hotkey):
-    kw.unhook_all()
-    kw.add_hotkey(new_hotkey, lambda: handle_hotkey(Controller()))
+    kb.unhook_all()
+    kb.add_hotkey(new_hotkey, lambda: handle_hotkey(Controller()))
 
 def quit():
-    # kw.press_and_release('esc')
-    kw.unhook_all()
+    # kb.press_and_release('esc')
+    kb.unhook_all()
     os._exit(0)
     exit(0)
 
 def main():
-    kw.add_hotkey('alt + shift + z', lambda: handle_hotkey(Controller()))
-    kw.wait()
+    kb.add_hotkey('alt + shift + z', lambda: handle_hotkey(Controller()))
+    kb.wait()
 
 if __name__ == '__main__':
     main()
